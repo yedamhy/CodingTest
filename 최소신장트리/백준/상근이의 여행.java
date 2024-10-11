fd996673151d5e4b343517ea1a7c1a007f95fb17
yedam <87516405+yedamhy@users.noreply.github.com>
2024-10-11 18:16:47 +0900

    백준 9372 - Silver4

---
+++ 최소신장트리/백준/상근이의 여행.java
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/9372
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int t=0; t<T; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for(int i=0; i<M; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+            }
+
+            System.out.println(N-1);
+        }
+    }
+}

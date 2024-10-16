31ec8823408b62932b7f61ef2023acfe005e2ba8
yedam <87516405+yedamhy@users.noreply.github.com>
2024-10-16 19:32:01 +0900

    Create 연간 평가점수에 해당하는 평가 등급 및 성과급 조회하기.sql

---
+++ SQL/GROUPBY/프로그래머스/연간 평가점수에 해당하는 평가 등급 및 성과급 조회하기.sql
@@ -0,0 +1,21 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/284528
+SELECT E.EMP_NO, EMP_NAME, 
+    CASE
+    WHEN AVG(SCORE) >= 96 THEN 'S'
+    WHEN AVG(SCORE) >= 90 THEN 'A'
+    WHEN AVG(SCORE) >= 80 THEN 'B'
+    ELSE  'C'
+    END AS GRADE,
+    
+    CASE
+    WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
+    WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
+    WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
+    ELSE 0
+    END AS BONUS
+    
+FROM HR_EMPLOYEES E
+LEFT JOIN HR_GRADE G
+ON E.EMP_NO = G.EMP_NO
+GROUP BY E.EMP_NO
+ORDER BY E.EMP_NO;
